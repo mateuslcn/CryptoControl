fd99209a3a0513020b04873a970f987af1ded1f3
Mateus Costa <51512582+mateuslcn@users.noreply.github.com>
2021-05-16 21:21:31 -0400

    historical balance of bitcoin contributions
    update removing login requirement via webscocket & fix inter/string problems

--- coinext_api_no_register.py
+++ coinext_api_no_register.py
@@ -5,6 +5,17 @@ import numpy as np
 from MongoDataBaseAccess import *
 
 nextIvalue = 0
+BTC = 1
+LTC = 2 
+ETH = 4 
+XRP = 6 
+BCH = 8 
+USDT = 10 
+LINK = 12 
+DOGE = 14 
+ADA = 16 
+EOS = 18
+XLM = 20
 
 def service_url(service_name):
   return 'https://api.coinext.com.br:8443/AP/%s' % service_name
@@ -17,55 +28,35 @@ def call_post(service_name, payload={}, **kwargs):
   res = requests.post(service_url(service_name), json.dumps(payload), **kwargs)
   return json.loads(res.content)
 
-def on_error(ws, error):
-  print(error)
+def show_info(cryptoCurrentValue):
+  print('Bitcoin Value:', cryptoCurrentValue)
+  cryptoBalance = personal_services(cryptoCurrentValue)
+  return cryptoBalance
 
-def show_info(ws, data_string):
-  data = json.loads(data_string)
-  print('Bitcoin Value:', data['o'][:].split(',')[4])
-  bitcoinCurrentValue = data['o'][:].split(',')[4]
-  print(personal_services(bitcoinCurrentValue))
-  print('TestingPoint')
-
-def start_talking(ws):
-  ws.on_message = show_info
-  call_service(ws, 'SubscribeTicker', {
-    "OMSId": 1, 
-    "InstrumentId": 1, # BTCBRL
-    "Interval": 60,    # 1 min
-    "IncludeLastCount": 10
-  })  
-
-def call_service(ws, service_name, data, level=0):
-  global nextIvalue
-  frame = {
-    'm': level,
-    'i': nextIvalue,
-    'n': service_name,
-    'o': json.dumps(data)
-  }
-  nextIvalue += 2
-  ws.send(json.dumps(frame))
-
-def personal_services (bitcoinCurrentValue):
-  buyedAt = [values.find_one()['buyedAt'][1:-1].split(',')[0], values.find_one()['buyedAt'][1:-1].split(',')[1]]
-  buyedQuantity = [values.find_one()['buyedQuantity'][1:-1].split(',')[0],values.find_one()['buyedQuantity'][1:-1].split(',')[1]]
-  realFortune = sum((bitcoinCurrentValue/np.array(buyedAt))*np.array(buyedQuantity)) - sum(np.array(buyedQuantity)) 
+def personal_services (cryptoCoin):
+  buyedAt = [int(values.find_one()['buyedAt'][1:-1].split(',')[0]), int(values.find_one()['buyedAt'][1:-1].split(',')[1])]
+  buyedQuantity = [int(values.find_one()['buyedQuantity'][1:-1].split(',')[0]),int(values.find_one()['buyedQuantity'][1:-1].split(',')[1])]
+  realFortune = sum((cryptoCoin/np.array(buyedAt))*np.array(buyedQuantity)) - sum(np.array(buyedQuantity))
   return realFortune
 
+def obterBooks(coin = BTC):
+  payload = {
+    'OMSId': 1,
+    'AccountId': 1,
+    'InstrumentId': coin,
+    'Depth': 1,
+    'StartIndex': 1,
+    "Interval": 60,
+    'EndDteTime': 151071922297021,
+    'BeginDateTime': 151071868195634,
+    'TradeTime': 151071868195634 #time.mktime(datetime.datetime(2020,5,10,18,52,47,874766).timetuple())
+  }
+  return call_post('GetL2Snapshot', payload)
+
 def main():
-  auth = call_get('authenticate', auth=(str(coinext_collection.find_one()['login']), str(coinext_collection.find_one()['password'])))
-  if auth['Authenticated']:
-    user_info = call_post('GetUserInfo', headers={
-      'aptoken': auth['Token'],
-      'Content-type': 'application/json'
-    })
-  print(user_info)
-  ws = websocket.WebSocketApp('wss://api.coinext.com.br/WSGateway/',
-                              on_error=on_error,
-                              on_message=show_info,
-                              on_open=start_talking)
-  ws.run_forever()
+  data=obterBooks()
+  bitcoinCurrentValue= data[0][4]
+  print('Bitcoin Balance:', show_info(bitcoinCurrentValue))
   
 if __name__ == "__main__":
   main()
