2da88eb44f2d272520a037896017ce0285442aef
Mateus Costa <51512582+mateuslcn@users.noreply.github.com>
2021-05-22 21:11:02 -0400

    Telebot+Multcoin

--- coinext_api_no_register.py
+++ coinext_api_no_register.py
@@ -1,21 +1,30 @@
 import json
 import requests
-import websocket # use "pip install websocket-client"
+#import websocket # use "pip install websocket-client"
 import numpy as np
 from MongoDataBaseAccess import *
+import yfinance as yf
+from pandas import json_normalize
+import pandas as pd
+import matplotlib
+matplotlib.use('TKAgg')
+import pandas as pd
+import telebot
 
 nextIvalue = 0
-BTC = 1
-LTC = 2 
-ETH = 4 
-XRP = 6 
-BCH = 8 
-USDT = 10 
-LINK = 12 
-DOGE = 14 
-ADA = 16 
-EOS = 18
-XLM = 20
+cryptodict = {
+  "BTC": "1",
+  "LTC": "2", 
+  "ETH": "4",
+  "XRP": "6", 
+  "BCH": "8", 
+  "USDT": "10",
+  "LINK": "12", 
+  "DOGE": "14", 
+  "ADA": "16", 
+  "EOS": "18",
+  "XLM": "20",
+}
 
 def service_url(service_name):
   return 'https://api.coinext.com.br:8443/AP/%s' % service_name
@@ -28,18 +37,40 @@ def call_post(service_name, payload={}, **kwargs):
   res = requests.post(service_url(service_name), json.dumps(payload), **kwargs)
   return json.loads(res.content)
 
-def show_info(cryptoCurrentValue):
-  print('Bitcoin Value:', cryptoCurrentValue)
-  cryptoBalance = personal_services(cryptoCurrentValue)
+def get_info(cryptoCurrentValue,cryptoCoin):
+  cryptoBalance = personal_services(cryptoCoin, cryptoCurrentValue)
   return cryptoBalance
 
-def personal_services (cryptoCoin):
-  buyedAt = [int(values.find_one()['buyedAt'][1:-1].split(',')[0]), int(values.find_one()['buyedAt'][1:-1].split(',')[1])]
-  buyedQuantity = [int(values.find_one()['buyedQuantity'][1:-1].split(',')[0]),int(values.find_one()['buyedQuantity'][1:-1].split(',')[1])]
-  realFortune = sum((cryptoCoin/np.array(buyedAt))*np.array(buyedQuantity)) - sum(np.array(buyedQuantity))
-  return realFortune
+def personal_services (cryptoCoin,cryptoCurrentValue):
 
-def obterBooks(coin = BTC):
+  if cryptoCoin == 'BTC':
+    buyedAt = [float(values.find_one()['buyedAt'][1:-1].split(',')[0]), float(values.find_one()['buyedAt'][1:-1].split(',')[1])]
+    buyedQuantity = [float(values.find_one()['buyedQuantity'][1:-1].split(',')[0]), float(values.find_one()['buyedQuantity'][1:-1].split(',')[1])]
+    realFortune = sum((cryptoCurrentValue/np.array(buyedAt))*np.array(buyedQuantity)) - sum(np.array(buyedQuantity))
+    return realFortune
+
+  if cryptoCoin == 'ETH':
+    buyedAt = [float(values.find_one()['buyedAt'][1:-1].split(',')[2])]
+    buyedQuantity = [float(values.find_one()['buyedQuantity'][1:-1].split(',')[2])]
+    realFortune = sum((cryptoCurrentValue/np.array(buyedAt))*np.array(buyedQuantity)) - sum(np.array(buyedQuantity))
+    return realFortune
+
+  if cryptoCoin == 'DOGE':
+    buyedAt = [float(values.find_one()['buyedAt'][1:-1].split(',')[3]), float(values.find_one()['buyedAt'][1:-1].split(',')[4]), float(values.find_one()['buyedAt'][1:-1].split(',')[5])]
+    buyedQuantity = [float(values.find_one()['buyedQuantity'][1:-1].split(',')[3]),float(values.find_one()['buyedQuantity'][1:-1].split(',')[4]), float(values.find_one()['buyedQuantity'][1:-1].split(',')[5])]
+    realFortune = sum((cryptoCurrentValue/np.array(buyedAt))*np.array(buyedQuantity)) - sum(np.array(buyedQuantity))
+    return realFortune
+  else:
+    return 'Coin Not Invested'
+
+def ticket_history(ticket):
+  
+  ticker = yf.Ticker(ticket)
+  start = '2021-01-01'
+  end = '2021-05-17'
+  return ticker.history(period='1d', interval ='1m')['Close']
+
+def obterBooks(coin):
   payload = {
     'OMSId': 1,
     'AccountId': 1,
@@ -53,10 +84,45 @@ def obterBooks(coin = BTC):
   }
   return call_post('GetL2Snapshot', payload)
 
+#Telegram bot mateusManaus_bot
+bot = telebot.TeleBot("1734494822:AAEV57FXwoqyBRYFWarnJoQgNMl-ILLqZlk")
+
 def main():
-  data=obterBooks()
-  bitcoinCurrentValue= data[0][4]
-  print('Bitcoin Balance:', show_info(bitcoinCurrentValue))
   
+  cryptoCoin = ['BTC', 'ETH', 'DOGE']
+  soma = []
+  report_partial = ''
+  title= 'CRYPTOCOIN INVESTMENT INSTANT REPORT \n\n'
+  print(title)
+
+  for x in cryptoCoin:
+    data=obterBooks(cryptodict[x])
+    cryptoCurrentValue= data[0][4]
+    print(x, 'Current Value:', '%.2f' % cryptoCurrentValue)
+    print('Current Balance:', '%.2f' % get_info(cryptoCurrentValue, x), '\n')
+    soma.append(get_info(cryptoCurrentValue, x))
+    report_p = str(x) + ' Current Value: R$' + str('%.2f' % cryptoCurrentValue) + \
+    '\nCurrent Balance: R$' + str('%.2f' % get_info(cryptoCurrentValue, x)) + '\n'
+    report_partial = report_partial + report_p + '\n'
+  
+  buyedQuantityTotal = [float(values.find_one()['buyedQuantity'][1:-1].split(',')[0]), float(values.find_one()['buyedQuantity'][1:-1].split(',')[1]), float(values.find_one()['buyedQuantity'][1:-1].split(',')[2]), float(values.find_one()['buyedQuantity'][1:-1].split(',')[3]), float(values.find_one()['buyedQuantity'][1:-1].split(',')[4]), float(values.find_one()['buyedQuantity'][1:-1].split(',')[5])]
+  print('Initial Investiment=', '%.2f' % sum(buyedQuantityTotal),'\n')
+  print('Current Loss=', '%.2f' % sum(soma),'\n')
+  print('Total Current Balance=', '%.2f' % (sum(buyedQuantityTotal)+sum(soma)),'\n')
+  report_total = 'Initial Investiment = R$' + str('%.2f' % sum(buyedQuantityTotal)) + '\n' \
+  'Current Loss = R$' + str('%.2f' % sum(soma)) + '\n' + \
+  'Total Current Balance = R$' + str('%.2f' % (sum(buyedQuantityTotal) + sum(soma))) + '\n\n'
+  report = title+report_partial+report_total 
+
+  @bot.message_handler(commands=['start', 'help'])
+  def send_welcome(message):
+	  bot.reply_to(message, report)
+
+  @bot.message_handler(func=lambda message: True)
+  def echo_all(message):
+	  bot.reply_to(message, message.text)
+    
 if __name__ == "__main__":
+  
   main()
+  bot.polling()
---
+++ tester_file.py
@@ -0,0 +1,42 @@
+from MongoDataBaseAccess import *
+import json
+import requests
+import numpy as np
+import yfinance as yf
+from pandas import json_normalize
+import pandas as pd
+import matplotlib
+import pandas as pd
+import matplotlib.pyplot as plt
+import telebot #pip install pyTelegramBotAPI
+
+def ticket_history(ticket):
+  
+  ticker = yf.Ticker(ticket)
+  start = '2021-01-01'
+  end = '2021-05-17'
+  data = ticker.history(period='1d', interval ='1m')
+  #data = ticker.history(period='1y', interval ='1d')['Close'].plot()
+  #data = ticker.history(interval='1d', start=start, end=end)['Close'].plot()
+
+  data_close = data['Close']
+  return data['Close']
+
+def main():
+  data_plot = ticket_history('USDBRL=X')
+  x =np.linspace(0,20,100)
+  #plt.plot(x,np.sin(x))
+  #plt.show()
+
+if __name__ == "__main__":
+  main()
+  x = sum([0,20,100]) 
+  message = 'dasdasd '+ str(sum([0,20,100])) + \
+  ' isso ae \n' + 'começo da 2alinha \n' + 'etc'
+  print(message)
+
+#print(str(coinext_collection.find_one()['login']))#testing srt
+#print('password: ', coinext_collection.find_one()['password'])
+
+#print('testing values',values.find_one()['buyedAt'][1:-1].split(',')[0], values.find_one()['buyedAt'][1:-1].split(',')[1])
+#print('testing values',values.find_one()['buyedQuantity'][1:-1].split(',')[0], values.find_one()['buyedQuantity'][1:-1].split(',')[1])
